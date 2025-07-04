ea5a70bbc0aee50e4b16d8c1906c7ffbcb53cc9a
FedorVD <fedorvd@mail.ru>
2025-07-05 00:32:59 +0300

    Initial commit

---
+++ src/test/java/org/top/airportdirectoryapp/AirportDirectoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.top.airportdirectoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportDirectoryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
